3f729c945a785d84526aebd713118028465c3501
mrkut0225 <167300313+mrkut0225@users.noreply.github.com>
2024-10-31 15:46:49 +0900

    Update create-LFuser.ps1

--- user_create-main/01_create-LFuser/create-LFuser.ps1
+++ user_create-main/01_create-LFuser/create-LFuser.ps1
@@ -1,3 +1,4 @@
 ﻿$password = ConvertTo-SecureString "lfmagicconnect" -AsPlainText -Force
 New-LocalUser -Name 'LFuser' -Password $password
+Set-LocalUser -Name 'LFuser' -PasswordNeverExpires $true
 Add-LocalGroupMember -Group "Users" -Member "LFuser"
